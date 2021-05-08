ec2c95ccfddff0f03cdfe0d3c94871ff37134e6e
Pesala-Hajarath-Babu <hajarath1998@gmail.com>
2021-05-08 15:55:23 +0530

    Hello World version 1.0

---
+++ hajju/src/hajju/HelloWorld.java
@@ -0,0 +1,9 @@
+package hajju;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		System.out.println("I am pushing my first project to Git hub");
+	}
+}
